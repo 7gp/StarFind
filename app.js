48266837f83dfc190f5a87a626c0d290e3417df4
7gp <63910973+7gp@users.noreply.github.com>
2025-01-26 16:39:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const webhookURL = "YOUR_DISCORD_WEBHOOK_URL"; // Replace with your webhook URL
+const webhookURL = "https://discord.com/api/webhooks/1333176484793290825/XoT2Ei0p-4Wz8-1HCP7-Z3QKPWanbJSNoVEE52nNvrCpEZFYRidtd_SneIrGip2RsvHa
+"; // Replace with your webhook URL
 let monitoring = false;
 let starReports = [];
 
